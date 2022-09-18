e66e5d83007e9bb728976bf341d3b1445e6b0666
Ekaterin <katrikacy@mail.ru>
2022-09-18 22:18:04 +0300

    Решение задачи 1 на нахождение большего числа

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число:");
+
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+   Console.WriteLine($"max={number1}");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine($"max={number2}");
+}
+else 
+{
+ Console.WriteLine($"Числа равны");
+}
