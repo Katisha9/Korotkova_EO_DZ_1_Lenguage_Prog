c7840e09e6e16c665d14670b31bfead6e4dc20c3
Ekaterin <katrikacy@mail.ru>
2022-09-18 22:50:41 +0300

    Решение задачи 4 - вывод четных от 1 до N

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ int number3 = int.Parse(Console.ReadLine());
 
 int max = Math.Max(number3, Math.Max(number1, number2));
 Console.WriteLine(max);
-*/
+
 
 // 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
@@ -44,4 +44,20 @@ if (number % 2 == 0)
 else
 {
   Console.WriteLine("Число является нечетным");
+}
+*/
+// 4.Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+int a = 1;
+
+while (a <= n)
+{
+    if (a % 2 == 0)
+    {
+        Console.WriteLine(a);
+    }
+    a++;
 }
